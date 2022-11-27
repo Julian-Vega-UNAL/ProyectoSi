9ba328b6bc7590b70bff0fc5e17f7f64e5b2cbf8
jtaquez <55629656+jtaquez@users.noreply.github.com>
2022-11-27 17:15:14 -0500

    Codigo para ejecutar final_path en ventana juego

--- src/do_move.py
+++ src/do_move.py
@@ -16,7 +16,7 @@ def run(movement):
 		move('right')
 	elif movement == "3":
 		move('left')
-
+""""
 #------------------------------------------------------------------
 chain = "3322221111133300033111222000220000000333331112220"
 
@@ -24,3 +24,33 @@ time.sleep(1) #Grace time
 for movement in chain:
 	print(movement)
 	run(movement)
+	
+	
+	import pyautogui as py
+import time
+import calculate_path
+#------------------------------------------------------------------
+def move(x):
+	py.keyDown(x)
+	time.sleep(0.35) #InputLag
+	py.keyUp(x)
+
+def run(movement):
+	if movement == "up":
+		move('up')
+	elif movement == "down":
+		move('down')
+	elif movement == "right":
+		move('right')
+	elif movement == "left":
+		move('left')
+
+	
+#------------------------------------------------------------------
+chain = final_path
+
+time.sleep(5) #Grace time
+for movement in chain:
+	print(movement)
+	run(movement)
+
