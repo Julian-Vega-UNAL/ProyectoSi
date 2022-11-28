8a9741257274e09791774b8b14cbb90e27b8eae5
jtaquez <55629656+jtaquez@users.noreply.github.com>
2022-11-27 20:25:01 -0500

    Create protomain_file.py

---
+++ src/protomain_file.py
@@ -0,0 +1,9 @@
+#### Aproche por Runpy
+
+#!/usr/bin/env python
+# coding: utf-8
+
+import runpy ### Toca cargar todas las librerias
+
+runpy.run_path(path_name='calculate_path.py')
+runpy.run_path(path_name='do_move.py') ## Mirar si funciona##
