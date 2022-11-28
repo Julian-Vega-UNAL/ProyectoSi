a7ce41f3d8e1f17b12c4f72efa496ac8bb5e36e5
jtaquez <55629656+jtaquez@users.noreply.github.com>
2022-11-27 20:35:29 -0500

    Update protomain_file.py

--- src/protomain_file.py
+++ src/protomain_file.py
@@ -5,5 +5,9 @@
 
 import runpy ### Toca cargar todas las librerias
 
+
+
+
+runpy.run_path(path_name='generate_field.py')
 runpy.run_path(path_name='calculate_path.py')
 runpy.run_path(path_name='do_move.py') ## Mirar si funciona##
